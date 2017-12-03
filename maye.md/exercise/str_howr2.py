aa927433d6d6d6a10655469876870a047a0f0823
1807136319@qq.com <1807136319@qq.com>
2017-12-03 13:44:14 +0800

    add maye.md file

--- maye.md/exercise/str_howr2.py
+++ maye.md/exercise/str_howr2.py
@@ -3,9 +3,13 @@ a = raw_input()
 m = 0
 b = []
 for i in a:
-    if 64 < ord(i) < 91 or 96 < ord(i) < 123:
+    if 64 < ord(i) < 90 or 96 < ord(i) < 122:
         m = ord(i) + 1
         sys.stdout.write(chr(m))
+    elif ord(i) == 90:
+        sys.stdout.write('A')
+    elif ord(i) == 122:
+        sys.stdout.write('a')
     else:
         sys.stdout.write(i)
 print
