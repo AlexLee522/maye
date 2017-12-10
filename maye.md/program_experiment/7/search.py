30a87778f25a20488f89614c2c9c530c27d00c3e
1807136319@qq.com <1807136319@qq.com>
2017-12-10 21:19:27 +0800

    add maye.md file

---
+++ maye.md/program_experiment/7/search.py
@@ -0,0 +1,11 @@
+import codecs
+name = input('请输入文件名：')
+from_file = codecs.open(name,encoding='utf-8').read()
+count = 0
+f = from_file.split()
+input_name = input('请输入要查找的姓氏：')
+for i in f:
+    if input_name in i:
+        print (i,end=' ')
+        count += 1
+print (count)
